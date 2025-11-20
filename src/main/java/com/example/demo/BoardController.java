c7df2dbcb00cf5fbaeac543b344bdf6873956827
infreeJ <kasious97@naver.com>
2025-11-20 15:44:41 +0900

    config 설정 및 @Cacheable 사용

---
+++ src/main/java/com/example/demo/BoardController.java
@@ -0,0 +1,23 @@
+package com.example.demo;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("boards")
+@RequiredArgsConstructor
+public class BoardController {
+    private final BoardService boardService;
+
+
+    @GetMapping()
+    public List<Board> getBoards(@RequestParam(defaultValue = "1") int page, @RequestParam(defaultValue = "10") int size) {
+        return boardService.getBoards(page, size);
+    }
+
+}
---
+++ src/main/java/com/example/demo/BoardService.java
@@ -0,0 +1,23 @@
+package com.example.demo;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@Service
+public class BoardService {
+    private final BoardRepository boardRepository;
+
+    @Cacheable(cacheNames = "getBoards", key = "'boards:page:' + #page + ':size:' + #size", cacheManager = "boardCacheManager")
+    public List<Board> getBoards(int page, int size) {
+        Pageable pageable = PageRequest.of(page - 1, size);
+        Page<Board> pageOfBoards = boardRepository.findAllByOrderByCreatedAtDesc(pageable);
+        return pageOfBoards.getContent();
+    }
+}
